2ba0e3d7930ba096b35c6c1a6ea2874fb193519a
buttercantfly <sml890115@gmail.com>
2025-03-19 07:28:06 -0700

    fix toggle password error message

--- public/javascripts/utility.js
+++ public/javascripts/utility.js
@@ -7,12 +7,14 @@ const checkDelete = () => {
 const togglePassword = document.querySelector('#togglePassword')
 const password = document.querySelector('#password')
 
-togglePassword.addEventListener('click', function (e) {
-  // toggle the type attribute
-  const type =
-    password.getAttribute('type') === 'password' ? 'text' : 'password'
-  password.setAttribute('type', type)
-  // toggle the eye / eye slash icon
-  this.classList.toggle('fa-eye')
-  this.classList.toggle('fa-eye-slash')
-})
+if(togglePassword && password) {
+  togglePassword.addEventListener('click', function (e) {
+    // toggle the type attribute
+    const type =
+      password.getAttribute('type') === 'password' ? 'text' : 'password'
+    password.setAttribute('type', type)
+    // toggle the eye / eye slash icon
+    this.classList.toggle('fa-eye')
+    this.classList.toggle('fa-eye-slash')
+  })
+}
